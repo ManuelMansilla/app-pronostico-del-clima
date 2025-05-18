e2955e968298c771ffd8299642d528e8075e537c
ManuelMansilla <Manuelmansilla111@gmail.com>
2025-05-18 20:21:42 -0300

    [FEAT] pronostico: se creo el componente Header

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './components/Header';
 import SearchView from './views/SearchView';
 import DetailView from './views/DetailView';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header title="Pronóstico del Clima" />
       <Routes>
         <Route path="/" element={<SearchView />} />
         <Route path="/detail" element={<DetailView />} />
